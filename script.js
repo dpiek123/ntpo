b48ea2701863234d3c067234cc466a4fbdbaa1b7
dpiek123 <damianpiekarski11@gmail.com>
2025-03-18 17:05:57 +0100

    tak

---
+++ script.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("addDiv")
+
+var number = 0;
+
+btn.addEventListener("click", () => {
+    let div = document.createElement('div')
+    let p = document.createElement('div')
+    let remove = document.createElement('div')
+
+    p.innerHTML = number
+    remove.innerHTML = '-'
+
+    div.append(p)
+    div.append(remove)
+
+    div.classList.add('newDiv')
+    remove.classList.add('remove')
+
+    remove.addEventListener("click", () => {
+        div.remove()
+
+        if(document.querySelector('.newDiv') == null){
+            number = 0
+        }
+    })
+
+    document.body.appendChild(div)
+    number++
+})
+
+console.log("yo")
